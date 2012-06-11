8e1e2fbcb98a65b6afe108a069318b3af38ab5bb
hash <hash@exultant.us>
2012-06-10 22:32:10 -0700

    moar tests

--- PsapTest.php
+++ PsapTest.php
@@ -11,17 +11,70 @@
 require_once "Psap.php";
 
 class PsapTest extends PHPUnit_Framework_TestCase {
-	public function testBasic() {
-		$parser = new Psap(array(
+	private static function setupAlpha() {
+		return new Psap(array(
 			'opt' => array(
 				'longname'	=> "option",
 				'shortname'	=> "o",
 				'description'	=> "desc",
 			),
 		));
+	}
+	
+	public function testBasic() {
+		$parser = self::setupAlpha();
 		$parser->parse(array());
 		$parser->result();
 		$parser->getErrors();
+	}
+	
+	public function testParseOneShortSeparatedString() {
+		$parser = self::setupAlpha();
+		$parser->parse(array("-o", "val"));
+		$this->assertEquals(array(), $parser->getErrors());
+		$this->assertEquals(
+			array("opt" => "val"),
+			$parser->result()
+		);
+	}
+	
+	public function testParseOneShortEqualledString() {
+		$parser = self::setupAlpha();
+		$parser->parse(array("-o=val"));
+		$this->assertEquals(
+			array("opt" => "val"),
+			$parser->result()
+		);
+	}
+	
+	public function testParseOneShortCattedString() {
+		$parser = self::setupAlpha();
+		$parser->parse(array("-oval"));
+		$this->assertEquals(array(), $parser->getErrors());
+		$this->assertEquals(
+			array("opt" => "val"),
+			$parser->result()
+		);
+	}
+	
+	public function testParseOneLongSeparatedString() {
+		$parser = self::setupAlpha();
+		$parser->parse(array("--option", "val"));
+		$this->assertEquals(array(), $parser->getErrors());
+		$this->assertEquals(
+			array("opt" => "val"),
+			$parser->result()
+		);
+	}
+	
+	public function testParseOneLongEqualledString() {
+		$parser = self::setupAlpha();
+		$parser->parse(array("--option=val"));
+		$this->assertEquals(array(), $parser->getErrors());
+		$this->assertEquals(
+			array("opt" => "val"),
+			$parser->result()
+		);
 	}
 }
 
